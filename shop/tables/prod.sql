49f428eeb0b147676e00de186765ca84f80e1f3b
Вадим <vadze_1@mail.ru>
2023-09-26 19:27:34 +0500

    add functions

--- shop/tables/prod.sql
+++ shop/tables/prod.sql
@@ -1,10 +1,10 @@
 CREATE TABLE shop.prod
 (
-    ID    INT       not null,
-    Dt    timestamp not null,
-    Cl_id INT       not null,
-    T_id  INT       not null,
-    Kol   INT       NOT NULL,
-    CONSTRAINT PK_prod PRIMARY KEY (ID),
-    CONSTRAINT ch_kol CHECK ( Kol > 0 )
+    id    INT       not null,
+    dt    timestamp not null,
+    cl_id INT       not null,
+    t_id  INT       not null,
+    kol   INT       NOT NULL,
+    CONSTRAINT PK_prod PRIMARY KEY (id),
+    CONSTRAINT ch_kol CHECK (kol > 0 )
 );
--- shop/tables/tovar.sql
+++ shop/tables/tovar.sql
@@ -1,11 +1,11 @@
 CREATE TABLE shop.tovar
 (
-    ID    INT            NOT NULL,
-    Naz   VARCHAR(30)    NOT NULL,
-    Price NUMERIC(10, 2) NOT NULL,
+    id    INT            NOT NULL,
+    naz   VARCHAR(30)    NOT NULL,
+    price NUMERIC(10, 2) NOT NULL,
     dt TIMESTAMPTZ NOT NULL,
     employee_id INT NOT NULL,
-    CONSTRAINT PK_tovar PRIMARY KEY (ID),
-    CONSTRAINT ch_price CHECK (Price > 0)
+    CONSTRAINT PK_tovar PRIMARY KEY (id),
+    CONSTRAINT ch_price CHECK (price > 0)
 );
 

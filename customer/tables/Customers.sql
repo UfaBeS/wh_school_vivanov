42f6069ed24fdfdbd323b26e8d0758c070241fc0
Вадим <vadze_1@mail.ru>
2023-10-08 20:18:19 +0500

    add functions

--- customer/tables/Customers.sql
+++ customer/tables/Customers.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS customer.customers
 (
-    customer_id BIGINT      NOT NULL
-        CONSTRAINT pk_customer_id PRIMARY KEY,
+    customer_id BIGINT      NOT NULL,
     name        VARCHAR(50) NOT NULL,
     phone       VARCHAR(15),
     vehicle_id  INT         NOT NULL,
-    CONSTRAINT uq_customer_vehicle UNIQUE (customer_id, vehicle_id)
+        CONSTRAINT pk_customer_vehicle PRIMARY KEY (customer_id, vehicle_id)
 );
--- service/tables/Services.sql
+++ service/tables/Services.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS service.services
 (
-    service_id   BIGINT         NOT NULL
-        CONSTRAINT pk_service_id PRIMARY KEY,
+    service_id   BIGINT         NOT NULL,
     service_name VARCHAR(500)   NOT NULL,
     price        DECIMAL(10, 2) NOT NULL, --для разных машин разная
     type_car     INT            NOT NULL,
-        CONSTRAINT uq_service_type_car UNIQUE (service_id, type_car)
+        CONSTRAINT pk_service_type_car PRIMARY KEY (service_id, type_car)
 );

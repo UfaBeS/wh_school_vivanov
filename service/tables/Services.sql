15813678c07ff8e9354299296ed5e455a5ad9961
UfaBeS <90246158+UfaBeS@users.noreply.github.com>
2023-10-09 14:49:57 +0500

    Update Services.sql
    Удалил лишнее

--- service/tables/Services.sql
+++ service/tables/Services.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS service.services
 (
     service_id   BIGINT         NOT NULL,
     service_name VARCHAR(500)   NOT NULL,
-    price        DECIMAL(10, 2) NOT NULL, --для разных машин разная
+    price        DECIMAL(10, 2) NOT NULL,
     type_car     INT            NOT NULL,
         CONSTRAINT pk_service_type_car PRIMARY KEY (service_id, type_car)
 );

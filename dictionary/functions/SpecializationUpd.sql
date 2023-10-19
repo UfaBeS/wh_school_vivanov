9bff1612926bfc29da04695639e5fa90d2eea97a
Вадим <vadze_1@mail.ru>
2023-10-19 13:12:16 +0500

    Мелкие исправления

--- dictionary/functions/SpecializationUpd.sql
+++ dictionary/functions/SpecializationUpd.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION dictionary.specilizationupd(_src JSONB) RETURNS JSONB
+CREATE OR REPLACE FUNCTION dictionary.specializationupd(_src JSONB) RETURNS JSONB
     SECURITY DEFINER
     LANGUAGE plpgsql
 AS

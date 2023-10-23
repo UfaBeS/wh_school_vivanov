9d891afcad2972bc5f0b5c0c15d25c0ec09c5415
Вадим <vadze_1@mail.ru>
2023-10-24 01:59:57 +0500

    Мелкие исправления

--- history/functions/CustomerChanges_CreatePartitions.sql
+++ history/functions/CustomerChanges_CreatePartitions.sql
@@ -1,22 +1,22 @@
-CREATE OR REPLACE FUNCTION history.customerchanges_createpartitions(start_date timestamptz, end_date timestamptz, table_name text) RETURNS VOID
-    LANGUAGE plpgsql
-    SECURITY DEFINER
+CREATE OR REPLACE FUNCTION history.customerchanges_createpartitions(_start_date TIMESTAMPTZ, _end_date TIMESTAMPTZ, _table_name TEXT) RETURNS VOID
+LANGUAGE plpgsql
+SECURITY DEFINER
 AS
 $$
 DECLARE
-    partition_name text;
-    current_partition_date date := start_date;
+    _partition_name TEXT;
+    _current_partition_date DATE := _start_date;
 BEGIN
     LOOP
-        EXIT WHEN current_partition_date >= end_date;
+        EXIT WHEN _current_partition_date >= _end_date;
 
-        partition_name := table_name || '_' || to_char(current_partition_date, 'YYYYMM');
+        _partition_name := _table_name || '_' || to_char(_current_partition_date, 'YYYYMM');
 
-        EXECUTE 'CREATE TABLE IF NOT EXISTS history.' || partition_name ||
-                ' PARTITION OF history.' || table_name ||
-                ' FOR VALUES FROM (''' || current_partition_date || ''') TO (''' || (current_partition_date + INTERVAL '1 month') || ''')';
+        EXECUTE 'CREATE TABLE IF NOT EXISTS history.' || _partition_name ||
+                ' PARTITION OF history.' || _table_name ||
+                ' FOR VALUES FROM (''' || _current_partition_date || ''') TO (''' || (_current_partition_date + INTERVAL '1 month') || ''')';
 
-        current_partition_date := current_partition_date + INTERVAL '1 month';
+        _current_partition_date := _current_partition_date + INTERVAL '1 month';
     END LOOP;
 END;
 $$;
--- history/functions/CustomerChanges_DeleteOldPartitions.sql
+++ history/functions/CustomerChanges_DeleteOldPartitions.sql
@@ -1,24 +1,25 @@
-CREATE OR REPLACE FUNCTION history.customerchanges_deleteoldpartitions() RETURNS void
-    LANGUAGE plpgsql AS
+CREATE OR REPLACE FUNCTION history.customerchanges_deleteoldpartitions(_table_name TEXT) RETURNS VOID
+LANGUAGE plpgsql
+AS
 $$
 DECLARE
-    old_partition_name text;
+    _old_partition_name TEXT;
 BEGIN
-    FOR old_partition_name IN
+    FOR _old_partition_name IN
         (WITH get_partexp AS
-                  (select pt.relname                                 as partition_name,
-                          pg_get_expr(pt.relpartbound, pt.oid, true) as partition_expression
-                   from pg_class base_tb
-                            join pg_inherits i on i.inhparent = base_tb.oid
-                            join pg_class pt on pt.oid = i.inhrelid
-                   where base_tb.oid = 'history.customerschanges'::regclass)
-         select partition_name
-         from get_partexp
-         where substring(partition_expression FROM 'TO \(''(.*?)''\)')::timestamptz < NOW() - INTERVAL '90 days')
-        LOOP
-            RAISE NOTICE 'Выполняем: DROP TABLE IF EXISTS history.customerschanges_%', old_partition_name;
-            EXECUTE 'DROP TABLE IF EXISTS history.' || old_partition_name;
-        END LOOP;
-
-END
+                  (SELECT pt.relname                                 AS partition_name,
+                          pg_get_expr(pt.relpartbound, pt.oid, true) AS partition_expression
+                   FROM pg_class base_tb
+                            JOIN pg_inherits i ON i.inhparent = base_tb.oid
+                            JOIN pg_class pt ON pt.oid = i.inhrelid
+                   WHERE base_tb.oid = concat('history.', _table_name)::regclass)
+         SELECT partition_name
+         FROM get_partexp
+         WHERE substring(partition_expression FROM 'TO \(''(.*?)''\)')::TIMESTAMPTZ < NOW() - INTERVAL '90 days')
+    LOOP
+        RAISE NOTICE 'Выполняем: DROP TABLE IF EXISTS history.customerschanges_%', _old_partition_name;
+        EXECUTE 'DROP TABLE IF EXISTS history.' || _old_partition_name;
+    END LOOP;
+END;
 $$;
+
